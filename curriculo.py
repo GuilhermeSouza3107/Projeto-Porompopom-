6f05c530dc98d4573e3d1ae6ab94f5c61d86afc9
GuilhermeSouza3107 <guilherme.s.pereira18@aluno.senai.br>
2025-11-19 09:19:35 -0300

    Itens necessarios
    [NOME]
    
    [CPF]
    
    [TELEFONE]
    
    [EMAIL]
    
    [ENDERECO]
    
    [OBJETIVO]
    
    [DESCRICAO_PESSOAL]
    
    [ULTIMA_EXPERIENCIA]
    
    [FUNCAO]
    
    [TEMPO_TRABALHO]
    
    [OUTRAS_EXPERIENCIAS]
    
    [HABILIDADES]
    
    [FORMACAO]
    
    [DISPONIBILIDADE]

---
+++ curriculo.py
@@ -0,0 +1,48 @@
+=========================================
+            CURRÍCULO PROFISSIONAL
+=========================================
+
+NOME COMPLETO: [NOME]
+CPF: [CPF]
+TELEFONE: [TELEFONE]
+E-MAIL: [EMAIL]
+ENDEREÇO: [ENDERECO]
+
+-----------------------------------------
+OBJETIVO PROFISSIONAL
+-----------------------------------------
+[OBJETIVO]
+
+-----------------------------------------
+SOBRE MIM
+-----------------------------------------
+[DESCRICAO_PESSOAL]
+
+-----------------------------------------
+EXPERIÊNCIAS PROFISSIONAIS
+-----------------------------------------
+Última experiência: [ULTIMA_EXPERIENCIA]
+Função exercida: [FUNCAO]
+Tempo de trabalho: [TEMPO_TRABALHO]
+
+Outras experiências relevantes:  
+[OUTRAS_EXPERIENCIAS]
+
+-----------------------------------------
+HABILIDADES
+-----------------------------------------
+[HABILIDADES]
+
+-----------------------------------------
+FORMAÇÃO
+-----------------------------------------
+[FORMACAO]
+
+-----------------------------------------
+DISPONIBILIDADE
+-----------------------------------------
+[DISPONIBILIDADE]
+
+=========================================
+          FIM DO CURRÍCULO
+=========================================
